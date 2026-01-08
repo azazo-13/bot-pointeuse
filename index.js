2fb860344124326962e8702a3bd95238c3f9e11c
azazo-13 <123328267+azazo-13@users.noreply.github.com>
2026-01-08 04:32:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -197,8 +197,7 @@ if (SELF_URL) {
 }
 
 // --- Login Discord ---
-client.login(TOKEN).then(() => {
-  console.log("🔑 Tentative de connexion au bot Discord...");
-}).catch(err => {
-  console.error("❌ Impossible de se connecter au bot Discord :", err);
-});
+client.login(TOKEN)
+  .then(() => console.log("🔑 Tentative de connexion au bot Discord..."))
+  .catch(err => console.error("❌ Impossible de se connecter au bot Discord :", err));
+
